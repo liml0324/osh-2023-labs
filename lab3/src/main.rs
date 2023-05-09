d2e0aa744d16361d1d25f2c3a7f811d6cd45a124
Mulong Li <liml@mail.ustc.edu.cn>
2023-05-09 23:45:08 +0800

    Finished thread pool

--- lab3/src/main.rs
+++ lab3/src/main.rs
@@ -2,41 +2,121 @@ use std::{
     io, 
     io::{prelude::*, BufReader},
     fs,
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream}, vec,
+    thread,
+    sync::{mpsc::{self, Receiver}, Arc, Mutex},
+    env, fmt::{format, Debug},
 };
 
-fn main() {
-    let listener = loop{
-        let listener = TcpListener::bind("0.0.0.0:8000");
-        match listener {
-            Ok(listener) => {
-                println!("Established listener successfully!");
-                break listener},
-            Err(_) => {
-                println!("Unable to listen on 0.0.0.0:8000.");
-                println!("Enter Y/y to try again, or enter any other keys to exit.");
-                let mut choice = String::new();
-                io::stdin()
-                    .read_line(&mut choice)
-                    .expect("Failed to read line.");
-                let choice_str = choice.trim();
-                if choice_str == "Y" || choice_str == "y"{
-                    continue;
-                }
-                else {
-                    return;
-                }
-            }
-        }
-    };
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_tcp_stream(stream);
+impl ThreadPool {
+    fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);//the other part of the program will grantee that size is greater than 0
+        let (sender, receiver) = mpsc::channel::<Job>();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::<Worker>::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool { workers, sender }
+    }
+
+    fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).expect("Thread pool failed to send job.");
     }
 }
 
-fn handle_tcp_stream(mut stream: TcpStream){
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().expect("Thread failed to get lock.").recv().expect("Thread failed to receive job.");
+            //println!("Worker {id} got a job; executing.");
+            job();
+        });
+        Worker { id, thread }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    let mut debug = false;
+    let mut thread_num = 4;
+    let mut port = "8000";
+
+    for i in 1..args.len() {
+        if args[i] == "-d" {
+            println!("Debug mode is on.");
+            debug = true;
+        }
+        else if args[i] == "-t" {
+            if i+1 < args.len() {
+                thread_num = match args[i+1].parse::<usize>() {
+                    Ok(t) => t,
+                    Err(_) => {
+                        println!("Invalid thread number, use 4 as default thread number.");
+                        4
+                    }
+                };
+            }
+            else {
+                println!("Thread number can't be empty, use 4 as default thread number.");
+            }
+        }
+        else if args[i] == "-p" {
+            if i+1 < args.len() {
+                port = match args[i+1].parse::<usize>() {
+                    Ok(_) => args[i+1].as_str(),
+                    Err(_) => {
+                        println!("Invalid port number, use 8000 as default port.");
+                        "8000"
+                    }
+                };
+            }
+            else {
+                println!("Port number can't be empty, use 8000 as default port.");
+            }
+        }
+    }
+
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = TcpListener::bind(addr);
+    let listener = match listener {
+        Ok(listener) => {
+            println!("Established listener successfully on 0.0.0.0:{}.", port);
+            println!("Using {} threads.", thread_num);
+            listener},
+        Err(_) => {
+            println!("Unable to listen on 0.0.0.0:{}.", port);
+            return;
+        }
+    };
+
+    let thread_pool = ThreadPool::new(thread_num);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread_pool.execute(move || {
+            handle_tcp_stream(stream, debug);
+        });
+    }
+}
+
+fn handle_tcp_stream(mut stream: TcpStream, debug: bool){
     let buf_reader = BufReader::new(& mut stream);
     let mut http_request:Vec<String> = Vec::new();
     let buf_reader_lines = buf_reader.lines();//read request
@@ -44,7 +124,7 @@ fn handle_tcp_stream(mut stream: TcpStream){
         let new_line = match line {
             Ok(line) => line,
             Err(_) => {
-                handle_500(stream);
+                handle_500(stream, debug);
                 return;
             },
         };
@@ -53,13 +133,15 @@ fn handle_tcp_stream(mut stream: TcpStream){
         }
         http_request.push(new_line);//save request in vector
     }
-    println!("Request: {:#?}", http_request);
+    if debug {
+        println!("Request: {:#?}", http_request);
+    }
 
     let request_line:Option<&String> = http_request.get(0);
     let request_line = match request_line {
         Some(line) => line,
         None => {
-            handle_500(stream);
+            handle_500(stream, debug);
             return;
         },
     };
@@ -73,7 +155,7 @@ fn handle_tcp_stream(mut stream: TcpStream){
     //     request_line_vec.push(new_line);
     // }
     // if request_line_vec.len() != 3{
-    //     handle_500(stream);
+    //     handle_500(stream, &debug);
     //     return;
     // }
     let request_line = request_line.trim();
@@ -89,20 +171,20 @@ fn handle_tcp_stream(mut stream: TcpStream){
     };
 
     if first_space_pos == request_line.len() || last_space_pos == request_line.len() {//incomplete request line
-        handle_500(stream);
+        handle_500(stream, debug);
         return;
     }
     
     let method = request_line[..first_space_pos].trim();
 
     if method != "GET" {
-        handle_500(stream);
+        handle_500(stream, debug);
         return;
     }
 
     let path = request_line[first_space_pos..last_space_pos].trim();
     if path.len() == 0 {
-        handle_500(stream);
+        handle_500(stream, debug);
         return;
     }
 
@@ -112,7 +194,9 @@ fn handle_tcp_stream(mut stream: TcpStream){
         path.remove(0);
     }
 
-    println!("Path: {}", path);
+    if debug { 
+        println!("Path: {}", path); 
+    }
 
     let last_dot_pos = path.rfind('.');
     let last_dot_pos = match last_dot_pos {
@@ -127,27 +211,22 @@ fn handle_tcp_stream(mut stream: TcpStream){
     }
 
     if path.len() == 0 {
-        handle_404(stream);
+        handle_404(stream, debug);
     }
     else if last_dot_pos < path.len()-1 && file_type == "html" || file_type == "txt"
         || file_type == "htm" || file_type == "css" || file_type == "js" {
         let contents = fs::read_to_string(path);
         match contents {
             Ok(contents) => {
-                let status_line = String::from("HTTP/1.0 200 OK");
-                let length = contents.len();
-                let response =
-                    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-                stream.write_all(response.as_bytes()).unwrap();
-                println!("Respond: [\r\n{response}\r\n]");
+                handle_200_string(stream, contents, debug);
             },
             Err(error) => {//file not found
                 if error.kind() == std::io::ErrorKind::NotFound {
-                    handle_404(stream);
+                    handle_404(stream, debug);
                     return;
                 }
                 else {
-                    handle_500(stream);
+                    handle_500(stream, debug);
                     return;
                 }
             },
@@ -157,21 +236,15 @@ fn handle_tcp_stream(mut stream: TcpStream){
         let contents = fs::read(path);
         match contents {
             Ok(contents) => {
-                let status_line = String::from("HTTP/1.0 200 OK");
-                let length = contents.len();
-                let response =
-                    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
-                stream.write_all(response.as_bytes()).unwrap();
-                stream.write_all(&contents).unwrap();
-                println!("Respond: [\r\n{response}\r\n]");
+                handle_200_u8(stream, contents, debug);
             },
             Err(error) => {//file not found
                 if error.kind() == std::io::ErrorKind::NotFound {
-                    handle_404(stream);
+                    handle_404(stream, debug);
                     return;
                 }
                 else {
-                    handle_500(stream);
+                    handle_500(stream, debug);
                     return;
                 }
             },
@@ -179,18 +252,45 @@ fn handle_tcp_stream(mut stream: TcpStream){
     }
 }
 
-fn handle_404(mut stream: TcpStream) {
+fn handle_404(mut stream: TcpStream, debug: bool) {
     let content = "404 NOT FOUND";
     let response = format!("HTTP/1.0 404 NOT FOUND \r\nContent-Length: {}\r\n\r\n{}",
         content.len(), content);
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("Respond: [\r\n{response}\r\n]");
+    stream.write_all(response.as_bytes());
+    if debug {
+        println!("Respond: [\r\n{response}\r\n]");
+    }
 }
 
-fn handle_500(mut stream: TcpStream) {
+fn handle_500(mut stream: TcpStream, debug: bool) {
     let content = "500 Internal Server Error";
     let response = format!("HTTP/1.0 500 Internal Server Error \r\nContent-Length: {}\r\n\r\n{}",
         content.len(), content);
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("Respond: [\r\n{response}\r\n]");
+    stream.write_all(response.as_bytes());
+    if debug {
+        println!("Respond: [\r\n{response}\r\n]");
+    }
+}
+
+fn handle_200_u8(mut stream: TcpStream, contents: Vec<u8>, debug: bool) {
+    let status_line = String::from("HTTP/1.0 200 OK");
+    let length = contents.len();
+    let response =
+    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
+    stream.write_all(response.as_bytes());
+    stream.write_all(&contents);
+    if debug {
+        println!("Respond: [\r\n{response}\r\n]");
+    }
+}
+
+fn handle_200_string(mut stream: TcpStream, contents: String, debug: bool) {
+    let status_line = String::from("HTTP/1.0 200 OK");
+    let length = contents.len();
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes());
+    if debug {
+        println!("Respond: [\r\n{response}\r\n]");
+    }
 }
