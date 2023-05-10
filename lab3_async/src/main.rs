fdc34c9cc15cc297f953aadb4ee346c797bf0541
Mulong Li <liml@mail.ustc.edu.cn>
2023-05-10 20:49:44 +0800

    Cargo new for lab3_async

---
+++ lab3_async/src/main.rs
@@ -0,0 +1,196 @@
+use std::{
+    io, 
+    io::{prelude::*, BufReader},
+    fs,
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = loop{
+        let listener = TcpListener::bind("0.0.0.0:8000");
+        match listener {
+            Ok(listener) => {
+                println!("Established listener successfully!");
+                break listener},
+            Err(_) => {
+                println!("Unable to listen on 0.0.0.0:8000.");
+                println!("Enter Y/y to try again, or enter any other keys to exit.");
+                let mut choice = String::new();
+                io::stdin()
+                    .read_line(&mut choice)
+                    .expect("Failed to read line.");
+                let choice_str = choice.trim();
+                if choice_str == "Y" || choice_str == "y"{
+                    continue;
+                }
+                else {
+                    return;
+                }
+            }
+        }
+    };
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_tcp_stream(stream);
+    }
+}
+
+fn handle_tcp_stream(mut stream: TcpStream){
+    let buf_reader = BufReader::new(& mut stream);
+    let mut http_request:Vec<String> = Vec::new();
+    let buf_reader_lines = buf_reader.lines();//read request
+    for line in buf_reader_lines {
+        let new_line = match line {
+            Ok(line) => line,
+            Err(_) => {
+                handle_500(stream);
+                return;
+            },
+        };
+        if new_line.is_empty() {
+            break;
+        }
+        http_request.push(new_line);//save request in vector
+    }
+    println!("Request: {:#?}", http_request);
+
+    let request_line:Option<&String> = http_request.get(0);
+    let request_line = match request_line {
+        Some(line) => line,
+        None => {
+            handle_500(stream);
+            return;
+        },
+    };
+    // let request_line = request_line.split(" ");
+    // let mut request_line_vec:Vec<&str> = Vec::new();
+    // for line in request_line {
+    //     if line.is_empty() {
+    //         break;
+    //     }
+    //     let new_line = line.trim();
+    //     request_line_vec.push(new_line);
+    // }
+    // if request_line_vec.len() != 3{
+    //     handle_500(stream);
+    //     return;
+    // }
+    let request_line = request_line.trim();
+    let first_space_pos = request_line.find(' ');
+    let last_space_pos = request_line.rfind(' ');
+    let first_space_pos = match first_space_pos {
+        Some(pos) => pos,
+        None => request_line.len(),
+    };
+    let last_space_pos = match last_space_pos {
+        Some(pos) => pos,
+        None => request_line.len(),
+    };
+
+    if first_space_pos == request_line.len() || last_space_pos == request_line.len() {//incomplete request line
+        handle_500(stream);
+        return;
+    }
+    
+    let method = request_line[..first_space_pos].trim();
+
+    if method != "GET" {
+        handle_500(stream);
+        return;
+    }
+
+    let path = request_line[first_space_pos..last_space_pos].trim();
+    if path.len() == 0 {
+        handle_500(stream);
+        return;
+    }
+
+    let mut path = path.to_string();
+
+    if path[0..1].to_string() == "/" {
+        path.remove(0);
+    }
+
+    println!("Path: {}", path);
+
+    let last_dot_pos = path.rfind('.');
+    let last_dot_pos = match last_dot_pos {
+        Some(pos) => pos,
+        None => path.len(),
+    };
+
+    let mut file_type = "";
+
+    if path.len() > 0 && last_dot_pos < path.len()-1 {
+        file_type = path[last_dot_pos+1..].trim();
+    }
+
+    if path.len() == 0 {
+        handle_404(stream);
+    }
+    else if last_dot_pos < path.len()-1 && file_type == "html" || file_type == "txt"
+        || file_type == "htm" || file_type == "css" || file_type == "js" {
+        let contents = fs::read_to_string(path);
+        match contents {
+            Ok(contents) => {
+                let status_line = String::from("HTTP/1.0 200 OK");
+                let length = contents.len();
+                let response =
+                    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+                stream.write_all(response.as_bytes()).unwrap();
+                println!("Respond: [\r\n{response}\r\n]");
+            },
+            Err(error) => {//file not found
+                if error.kind() == std::io::ErrorKind::NotFound {
+                    handle_404(stream);
+                    return;
+                }
+                else {
+                    handle_500(stream);
+                    return;
+                }
+            },
+        };
+    }
+    else {
+        let contents = fs::read(path);
+        match contents {
+            Ok(contents) => {
+                let status_line = String::from("HTTP/1.0 200 OK");
+                let length = contents.len();
+                let response =
+                    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
+                stream.write_all(response.as_bytes()).unwrap();
+                stream.write_all(&contents).unwrap();
+                println!("Respond: [\r\n{response}\r\n]");
+            },
+            Err(error) => {//file not found
+                if error.kind() == std::io::ErrorKind::NotFound {
+                    handle_404(stream);
+                    return;
+                }
+                else {
+                    handle_500(stream);
+                    return;
+                }
+            },
+        };
+    }
+}
+
+fn handle_404(mut stream: TcpStream) {
+    let content = "404 NOT FOUND";
+    let response = format!("HTTP/1.0 404 NOT FOUND \r\nContent-Length: {}\r\n\r\n{}",
+        content.len(), content);
+    stream.write_all(response.as_bytes()).unwrap();
+    println!("Respond: [\r\n{response}\r\n]");
+}
+
+fn handle_500(mut stream: TcpStream) {
+    let content = "500 Internal Server Error";
+    let response = format!("HTTP/1.0 500 Internal Server Error \r\nContent-Length: {}\r\n\r\n{}",
+        content.len(), content);
+    stream.write_all(response.as_bytes()).unwrap();
+    println!("Respond: [\r\n{response}\r\n]");
+}
---
+++ lab3_async/target/debug/deps/lab3_async-58a5d4ab3db4497d.d
@@ -0,0 +1,5 @@
+D:\OSH\osh-2023-labs\lab3_async\target\debug\deps\lab3_async-58a5d4ab3db4497d.rmeta: src\main.rs
+
+D:\OSH\osh-2023-labs\lab3_async\target\debug\deps\lab3_async-58a5d4ab3db4497d.d: src\main.rs
+
+src\main.rs:
